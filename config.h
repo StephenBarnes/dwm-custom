4ff09e8d4b6fd81a50c89c1de9a3cc358d8aa9dc
Stephen Barnes <stephen6174@gmail.com>
2025-10-04 18:32:07 +0200

    Add clipmenu

--- config.h
+++ config.h
@@ -69,50 +69,55 @@ static const char *downvol[] = { "wpctl", "set-volume", "@DEFAULT_AUDIO_SINK@", 
 static const char *mutevol[] = { "wpctl", "set-mute", "@DEFAULT_AUDIO_SINK@", "toggle", NULL };
 
 static const Key keys[] = {
-	// DEFAULTS
-	/* modifier                     key        function        argument */
-	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
-	{ MODKEY,                       XK_b,      togglebar,      {0} },
-	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
-	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
-	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
-	//{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } }, // I don't know what this is, seems to do nothing, commented out bc conflicts with dmenu.
-	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
-	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
-	{ MODKEY,                       XK_Return, zoom,           {0} },
-	{ MODKEY,                       XK_Tab,    view,           {0} },
-	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
-	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
-	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
-	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
-	{ MODKEY,                       XK_space,  setlayout,      {0} },
-	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
-	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
-	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
-	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } }, // Focus previous monitor
-	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } }, // Focus next monitor
-	{ MODKEY,                       XK_apostrophe, focusothermon, {0} }, // Focus other monitor, and warp pointer
-	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
-	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
-	TAGKEYS(                        XK_1,                      0)
-	TAGKEYS(                        XK_2,                      1)
-	TAGKEYS(                        XK_3,                      2)
-	TAGKEYS(                        XK_4,                      3)
-	TAGKEYS(                        XK_5,                      4)
-	TAGKEYS(                        XK_6,                      5)
-	TAGKEYS(                        XK_7,                      6)
-	TAGKEYS(                        XK_8,                      7)
-	TAGKEYS(                        XK_9,                      8)
-	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+	/* modifier, key, function, argument */
 
-	// CUSTOM
-    { MODKEY,                      XK_n,       spawn,    {.v = slockcmd } }, // Lock key for slock (screen locker). Lock key on keyboard is not a distinct keysym, it's seen as super+N. (Probably actually super+L, but Dvorak makes L->N.)
-    { MODKEY|ShiftMask,            XK_r,       quit,           {1} }, // Restart dwm with Mod+Shift+r
-	{ MODKEY,                       XK_d,      spawn,          {.v = dmenucmd } }, // Mod+d for dmenu
+	// Spawn terminal
+	{MODKEY | ShiftMask, XK_Return, spawn, {.v = termcmd}},
+	{MODKEY, XK_b, togglebar, {0}},
+
+	// TODO rebind these below to match dvorak vim bindings in nvimrc
+	{MODKEY, XK_j, focusstack, {.i = +1}},
+	{MODKEY, XK_k, focusstack, {.i = -1}},
+	{MODKEY, XK_i, incnmaster, {.i = +1}},
+	//{ MODKEY, XK_d, incnmaster, {.i = -1 } }, // Commented out bc conflicts with 
+
+	{MODKEY, XK_h, setmfact, {.f = -0.05}},
+	{MODKEY, XK_l, setmfact, {.f = +0.05}},
+
+	{MODKEY, XK_Return, zoom, {0}},
+	{MODKEY, XK_Tab, view, {0}},
+	{MODKEY | ShiftMask, XK_c, killclient, {0}},
+	{MODKEY, XK_t, setlayout, {.v = &layouts[0]}},
+	{MODKEY, XK_f, setlayout, {.v = &layouts[1]}},
+	{MODKEY, XK_m, setlayout, {.v = &layouts[2]}},
+	{MODKEY, XK_space, setlayout, {0}},
+	{MODKEY | ShiftMask, XK_space, togglefloating, {0}},
+	{MODKEY, XK_0, view, {.ui = ~0}},
+	{MODKEY | ShiftMask, XK_0, tag, {.ui = ~0}},
+	{MODKEY, XK_comma, focusmon, {.i = -1}},	 // Focus previous monitor
+	{MODKEY, XK_period, focusmon, {.i = +1}},	 // Focus next monitor
+	{MODKEY, XK_apostrophe, focusothermon, {0}}, // Focus other monitor, and warp pointer
+	{MODKEY | ShiftMask, XK_comma, tagmon, {.i = -1}},
+	{MODKEY | ShiftMask, XK_period, tagmon, {.i = +1}},
+	TAGKEYS(XK_1, 0) TAGKEYS(XK_2, 1) TAGKEYS(XK_3, 2) TAGKEYS(XK_4, 3) TAGKEYS(XK_5, 4) TAGKEYS(XK_6, 5) TAGKEYS(XK_7, 6) TAGKEYS(XK_8, 7) TAGKEYS(XK_9, 8)
+	{MODKEY | ShiftMask, XK_q, quit, {0}},
+
+	// Lock key for slock (screen locker). Lock key on keyboard is not a distinct keysym, it's seen as super+N. (Probably actually super+L, but Dvorak makes L->N.)
+	{MODKEY, XK_n, spawn, {.v = slockcmd}},
+
+	// Restart dwm with mod+shift+r -- this depends on .xinitrc being set up to run dwm while return code is 1
+	{MODKEY | ShiftMask, XK_r, quit, {1}},
+
+	// Mod+d for dmenu
+	{MODKEY, XK_d, spawn, {.v = dmenucmd}},
+
 	// Volume
-	{ 0, XF86XK_AudioRaiseVolume, spawn, {.v = upvol } },
-	{ 0, XF86XK_AudioLowerVolume, spawn, {.v = downvol } },
-	{ 0, XF86XK_AudioMute,        spawn, {.v = mutevol } },
+	{0, XF86XK_AudioRaiseVolume, spawn, {.v = upvol}},
+	{0, XF86XK_AudioLowerVolume, spawn, {.v = downvol}},
+	{0, XF86XK_AudioMute, spawn, {.v = mutevol}},
+
+	// Clipmenu (clipboard manager) with mod+v
+	{MODKEY, XK_v, spawn, SHCMD("clipmenu")},
 };
 
 /* button definitions */
